1171da26becef331cb16c56edc5c07df667614c7
Jordan Hayes <hayesjordan@gmail.com>
2021-04-16 12:36:08 -0700

    Added verbose logging

--- pipeline/assemble/02_plasmidspades.sh
+++ pipeline/assemble/02_plasmidspades.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/bash
 #SBATCH -p intel,batch -N 1 -n 16 --mem 64gb --out logs/plasmidspades.%a.log -J plasmidspades
 
-module load spades/3.15.2
+# Load modules
+if [ -n "$MODULESHOME" ]; then
+  module load spades/3.15.2
+fi
 
+# Define params
 MEM=64
 SAMPLES=samples_prefix.csv
 INFOLDER=input
 ASM=assembly
+
+# Ensure parent output directory
 mkdir -p $ASM
+
+# Determine num CPUs
 CPU=$SLURM_CPUS_ON_NODE
 if [ -z $CPU ]; then
   CPU=$2
@@ -16,8 +24,8 @@ if [ -z $CPU ]; then
   fi
 fi
 
+# Determine job index
 N=${SLURM_ARRAY_TASK_ID}
-
 if [ ! $N ]; then
     N=$1
     if [ ! $N ]; then
@@ -26,22 +34,31 @@ if [ ! $N ]; then
     fi
 fi
 
-
-#--meta
+# Determine input data from job index
 IFS=,
-tail -n +2 $SAMPLES | sed -n ${N}p | while read SPECIES STRAIN JGILIBRARY BIOSAMPLE BIOPROJECT TAXONOMY_ID ORGANISM_NAME SRA_SAMPID SRA_RUNID LOCUSTAG TEMPLATE
-do
+tail -n +2 $SAMPLES | sed -n ${N}p | while read SPECIES STRAIN JGILIBRARY BIOSAMPLE BIOPROJECT TAXONOMY_ID ORGANISM_NAME SRA_SAMPID SRA_RUNID LOCUSTAG TEMPLATE; do
+
+  # Determine output directory
   STEM=$(echo -n $SPECIES | perl -p -e 's/\s+/_/g')
   OUTFOLDER=$ASM/${STEM}.plasmidspades
+  echo -e "OUTPUT:\n\t${OUTFOLDER}"
+  
+  # Run spades
   if [ ! -d $OUTFOLDER ]; then
-    time spades.py --meta --plasmid --threads $CPU -m $MEM \
+    cmd="spades.py --meta --plasmid --threads $CPU -m $MEM \
         -1 ${INFOLDER}/${STEM}_R1.fq.gz -2 ${INFOLDER}/${STEM}_R2.fq.gz \
-        -o $OUTFOLDER
+        -o $OUTFOLDER"
+    echo -e "Running:\n\t$cmd"
+    time $cmd
   fi
+  
+  # Clean up and compress
   if [ -f $OUTFOLDER/scaffolds.fasta ]; then
+    echo -e "Cleaning..."
     rm -rf $OUTFOLDER/before_rr.fasta $OUTFOLDER/corrected $OUTFOLDER/K*
     rm -rf $OUTFOLDER/assembly_graph_after_simplification.gfa $OUTFOLDER/tmp
     pigz $OUTFOLDER/contigs.fasta
     pigz $OUTFOLDER/spades.log
   fi
+  
 done
