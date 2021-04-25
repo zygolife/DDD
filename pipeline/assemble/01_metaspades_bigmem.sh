46f4afa861ccf28192e688730d20c8c5402f339a
Jason Stajich <jasonstajich.phd@gmail.com>
2021-04-25 16:28:50 -0700

    add big mem run

---
+++ pipeline/assemble/01_metaspades_bigmem.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+#SBATCH -p intel,batch -N 1 -n 16 --mem 140gb --out logs/metaspades_bigmem.%a.log -J metaspades
+
+module load spades/3.15.2
+
+MEM=140
+SAMPLES=samples_prefix.csv
+INFOLDER=input
+ASM=assembly
+mkdir -p $ASM
+CPU=$SLURM_CPUS_ON_NODE
+if [ -z $CPU ]; then
+  CPU=1
+fi
+
+N=${SLURM_ARRAY_TASK_ID}
+
+if [ ! $N ]; then
+    N=$1
+    if [ ! $N ]; then
+        echo "Need an array id or cmdline val for the job"
+        exit
+    fi
+fi
+
+
+#--meta
+IFS=,
+tail -n +2 $SAMPLES | sed -n ${N}p | while read SPECIES STRAIN JGILIBRARY BIOSAMPLE BIOPROJECT TAXONOMY_ID ORGANISM_NAME SRA_SAMPID SRA_RUNID LOCUSTAG TEMPLATE
+do
+  STEM=$(echo -n $SPECIES | perl -p -e 's/\s+/_/g')
+  OUTFOLDER=$ASM/${STEM}.spades
+  if [ ! -d $OUTFOLDER ]; then
+   time spades.py --meta --threads $CPU -m $MEM \
+        -1 ${INFOLDER}/${STEM}_R1.fq.gz -2 ${INFOLDER}/${STEM}_R2.fq.gz \
+        -o $OUTFOLDER
+  elif [ ! -f $OUTFOLDER/scaffolds.fasta ]; then
+	  time spades.py --threads $CPU -m $MEM -o $OUTFOLDER --restart-from last
+  fi	
+  if [ -f $OUTFOLDER/scaffolds.fasta ]; then
+    rm -rf $OUTFOLDER/tmp $OUTFOLDER/corrected $OUTFOLDER/K*
+    rm -f $OUTFOLDER/before_rr.fasta $OUTFOLDER/first_pe_contigs.fasta
+    pigz $OUTFOLDER/spades.log $OUTFOLDER/*.gfa $OUTFOLDER/*.fastg $OUTFOLDER/*.paths
+  fi
+done
