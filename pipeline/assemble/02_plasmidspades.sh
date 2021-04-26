0bf8862323b168b51c79d711d503b8505744e304
Jordan Hayes <hayesjordan@gmail.com>
2021-04-26 16:27:26 -0700

    Fixed closing if

--- pipeline/assemble/02_plasmidspades.sh
+++ pipeline/assemble/02_plasmidspades.sh
@@ -45,17 +45,15 @@ tail -n +2 $SAMPLES | sed -n ${N}p | while read SPECIES STRAIN JGILIBRARY BIOSAM
   
   # Run spades with either --meta or --plasmid
   if [[ ! -d $OUTFOLDER && ! -f $OUTFOLDER/scaffolds.fasta ]]; then
-  	if [ -d $OUTFOLDER ]; then
-     echo "Restarting spades.py --plasmid -o $OUTFOLDER"
-		 time spades.py --threads $CPU -o $OUTFOLDER --restart-from last
-	else
-    echo "Running spades.py --plasmid --threads $CPU -m $MEM -1 ${INFOLDER}/${STEM}_R1.fq.gz -2 ${INFOLDER}/${STEM}_R2.fq.gz -o $OUTFOLDER"
-    time spades.py --plasmid --threads $CPU -m $MEM -1 ${INFOLDER}/${STEM}_R1.fq.gz -2 ${INFOLDER}/${STEM}_R2.fq.gz -o $OUTFOLDER
-    
-    		time spades.py --meta --plasmid --threads $CPU -m $MEM \
-        -1 ${INFOLDER}/${STEM}_R1.fq.gz -2 ${INFOLDER}/${STEM}_R2.fq.gz \
-        -o $OUTFOLDER
-	fi  
+    if [ -d $OUTFOLDER ]; then
+      echo "Restarting spades.py --plasmid -o $OUTFOLDER"
+      time spades.py --threads $CPU -o $OUTFOLDER --restart-from last
+    else
+      echo "Running spades.py --plasmid --threads $CPU -m $MEM -1 ${INFOLDER}/${STEM}_R1.fq.gz -2 ${INFOLDER}/${STEM}_R2.fq.gz -o $OUTFOLDER"
+      time spades.py --plasmid --threads $CPU -m $MEM -1 ${INFOLDER}/${STEM}_R1.fq.gz -2 ${INFOLDER}/${STEM}_R2.fq.gz -o $OUTFOLDER
+    fi
+  fi
+
   # Clean up and compress
   if [ -f $OUTFOLDER/scaffolds.fasta ]; then
     echo "Cleaning..."
