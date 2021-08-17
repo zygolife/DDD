5f8805e75ce6bbe766208dfa64e09d8adb9608e2
Jason Stajich <jasonstajich.phd@gmail.com>
2021-08-17 12:47:53 -0700

    prep updated for 3 different extractions

--- pipeline/BTL/00_prep_search.sh
+++ pipeline/BTL/00_prep_search.sh
@@ -4,9 +4,17 @@
 
 module load ncbi-blast/2.9.0+
 
+
+DB=results/combined_db/autometa_Bacteria.merge.fa
+cat results/autometa/*/Bacteria.fasta > $DB
+makeblastdb -in $DB -dbtype nucl -title DDD_autometa_Bacteria
+
+DB=results/combined_db/autometa_filtered.merge.fa
+cat results/autometa/*/Bacteria_run/Bacteria.filtered.fasta > $DB
+makeblastdb -in $DB -dbtype nucl -title DDD_autometa_Bacteria_filtered
+
 DB=results/combined_db/genome_asm.merge.fa
 cat results/genome_asm/*.merge.fa > $DB
-
 makeblastdb -in $DB -dbtype nucl -title DDD_merge
 
 
